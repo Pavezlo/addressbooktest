7555a370617cc2f0b9570e38c3fcbab49d5c3665
Pavezlo <Pavelzlo18@yandex.ru>
2022-02-23 15:56:19 +0300

    Изменения согласно лекции 2.1

---
+++ addressbooktest/GroupCretionTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class GroupCretionTests : TestBase
+    {
+
+        [Test]
+        public void GroupCreationTest()
+        {
+            GoToHomePage();
+            Login(new AccountData("admin","secret"));
+            GoToGroupsPage();
+            InitNewGroupCreation();
+            GroupData group = new GroupData("aaa");
+            group.Header = "ddd";
+            group.Footer = "ddd";
+            FillGroupForm(group);
+            SubmitGroupCreation();
+            ReturnToGroupPage();
+        }
+    }
+}
