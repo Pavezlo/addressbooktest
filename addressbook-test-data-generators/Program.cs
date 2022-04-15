7fd415fdfcaed2463a46b157e29a078025abb5cb
Pavezlo <Pavelzlo18@yandex.ru>
2022-04-15 16:29:15 +0300

    Правки огласно лекции 6.1

---
+++ addressbook-test-data-generators/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using WebAddressbookTests;
+
+namespace addressbook_test_data_generators
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int count = Convert.ToInt32(args[0]);
+            StreamWriter writer = new StreamWriter(args[1]);
+            for (int i = 0; i < count; i++)
+            {
+                writer.WriteLine(String.Format("${0},${1},${2}",TestBase.GenerateRandomString(10),TestBase.GenerateRandomString(10),TestBase.GenerateRandomString(10)));
+            }
+            writer.Close();
+        }
+    }
+}
+
